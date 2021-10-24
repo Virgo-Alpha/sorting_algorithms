90c9645ba1c3260b32a966f1c02a7a98632c94c4
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-10-24 13:13:13 +0400

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,88 +1,67 @@
 #include "sort.h"
 
 /**
- * quick_sort - function that sorts an array of ints in ascending order
- * using quick sort algorithm
- * @array: list of numbers
+ * part - partitions an array at given pivot
+ * @array: array of integers
+ * @start: starting index.
+ * @end: ending index.
  * @size: size of array
+ *
+ * Return: pivot Index.
  */
-
-void quick_sort(int *array, size_t size)
+int part(int *array, int start, int end, size_t size)
 {
-	int low;
-	int high;
+	int i, pIndex = start, pivot = array[end], temp = 0;
 
-	if (array == NULL || size == 0)
-		return;
-
-	low = 0;
-	high = size - 1;
-
-	my_sort(array, low, high, size);
-}
-
-/**
- * my_sort - sorts array using low / high positions
- * @array: list of numbers
- * @low: first index of array
- * @high: last index of array
- * @size: size of array
- */
-void my_sort(int *array, int low, int high, size_t size)
-{
-	int p;
-
-	if (low < high)
+	for (i = start; i < end; i++)
 	{
-		p = partition(array, low, high, size);
-		my_sort(array, low, p - 1, size);
-		my_sort(array, p + 1, high, size);
-	}
-}
-
-/**
- * partition - sections an array using Lomuto quick_sort algo
- * @array: list of integers
- * @low: first index of array
- * @high: lowest index of array
- * @size: size of array
- * Return: position of pivot in array
- */
-int partition(int *array, int low, int high, size_t size)
-{
-	int pivot;
-	int i;
-	int j;
-
-	pivot = array[high];
-	i = low - 1;
-
-	for (j = low; j <= high; j++)
-	{
-		if (array[j] <= pivot)
+		if (array[i] <= pivot)
 		{
-			i++;
-			if (i != j)
-			{
-				swap(array, i, j);
-				print_array(array, size);
-			}
+			temp = array[i];
+			array[i] = array[pIndex];
+			array[pIndex] = temp;
+			pIndex++;
+			print_array(array, size);
 		}
 	}
-	return (i);
+	temp = array[pIndex];
+	array[pIndex] = array[end];
+	array[end] = temp;
+
+	return (pIndex);
 }
 
 /**
- * swap - swap values in array
- * @array: list of ints
- * @i: first element to swap
- * @j: second element to swap
+ * quicksort_r - recursively calls part to sort an array of integers.
+ * @array: array of integers
+ * @start: start index
+ * @end: end index
+ * @size: size of array
  */
-void swap(int *array, int i, int j)
+void quicksort_r(int *array, int start, int end, size_t size)
 {
-	int temp;
+	int  pIndex = 0;
 
-	temp = array[i];
-	array[i] = array[j];
-	array[j] = temp;
+	if (start < end)
+	{
+		pIndex = part(array, start, end, size);
+		quicksort_r(array, start, pIndex - 1, size);
+		quicksort_r(array, pIndex + 1, end, size);
+	}
+}
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using the
+ * quick sort algorithm.
+ *
+ * @array: an array of integers.
+ * @size: size of array.
+ *
+ * Return: void.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || !size)
+		return;
+	quicksort_r(array, 0, (int)size - 1, size);
 }
